f582ed7078851f359488d093fbf9e7746aa4d199
niwong <nick@nickwong.io>
2019-03-17 21:58:13 -0700

    added hecka stuff and got the scroll feature to work

---
+++ assets/scripts/app.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+	var elems = document.querySelectorAll('.scrollspy');
+	var options = {}
+	var instances = M.ScrollSpy.init(elems, options);
+});
